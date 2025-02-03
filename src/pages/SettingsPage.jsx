c4cfb95b78dd5458e057ed549cae6896583ba5a1
cccmmmd <bblisme8b@gmail.com>
2025-02-03 16:24:30 +0800

    Setting page

---
+++ src/pages/SettingsPage.jsx
@@ -0,0 +1,20 @@
+import Header from "../components/common/Header";
+import ConnectedAccounts from "../components/settings/ConnectedAccounts";
+import DangerZone from "../components/settings/DangerZone";
+import Notifications from "../components/settings/Notifications";
+import Security from "../components/settings/Security";
+
+const SettingsPage = () => {
+	return (
+		<div className='flex-1 overflow-auto relative z-10'>
+			<Header title='Settings' />
+			<main className='max-w-4xl mx-auto py-6 px-4 lg:px-8'>
+				<Notifications />
+				<Security />
+				<ConnectedAccounts />
+				<DangerZone />
+			</main>
+		</div>
+	);
+};
+export default SettingsPage;
